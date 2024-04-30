17df78cf5e525140d8f48f01b2a09437494c79ff
Justin Rodriguez <justin.r@me.com>
2024-04-29 23:28:23 -0400

    complete lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,21 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for i in range(len(dwarves)):
+        print(f"{i + 1}. {dwarves[i]}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(calls):
+    return [call.title() + "!" for call in calls]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(foods):
+    cheeses = ['cheddar', 'gouda', 'camembert']
+
+    for food in foods:
+        if food in cheeses:
+            return food
+        
+    return None
